2d868fce606e02678b5f53bc78493a3f89d12aae
MinCarve <artem.starikovich@gmail.com>
2017-07-27 10:21:30 -0700

    Add files via upload

--- Siri.py
+++ Siri.py
@@ -3,24 +3,37 @@ import random
 import datetime
 
 browserSearch = False
+youtubeSearch = False
 time = datetime.datetime.now()
-search = "search"
-i = random.randint(0, 1)
-options = [time, search]
+gsearch = "search"
+ytsearch = "ytsearch"
+i = random.randint(0, 2)
+options = [time, ytsearch, gsearch]
 
 _options = options[i]
 
 if _options == time:
-    browserSearch = False
+    print("The date and time is")
     print(time)
 
-elif _options == search:
+elif _options == gsearch:
     browserSearch = True
+
+elif _options == ytsearch:
+    youtubeSearch = True
+
+if youtubeSearch == True:
+    yturl = "https://youtube.com/results?search_query="
+
+    ytsearch = input("What video do you want to watch?")
+    ytsearch = '+'.join(ytsearch.split())
+    
+    webbrowser.open(yturl + ytsearch)
 
 if browserSearch == True:
     browserurl = "https://google.com/#q="
 
-    search = input("Enter a keyword to search \n")
+    search = input("Enter a keyword to search the web \n")
     search = '+'.join(search.split())
 
     webbrowser.open(browserurl + search)
