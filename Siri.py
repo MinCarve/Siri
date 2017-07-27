365eb02ddd1bd03e5b71bb9d548f9197e0a5072b
MinCarve <artem.starikovich@gmail.com>
2017-07-26 17:30:43 -0700

    Add files via upload

--- Siri.py
+++ Siri.py
@@ -1,13 +1,7 @@
 import webbrowser
 import random
-import pyttsx
-
-engine = pyttsx.init()
 
 browserurl = "https://google.com/#q="
-
-engine.say("Enter a keyword to search")
-engine.runAndWait()
 
 search = input("Enter a keyword to search \n")
 
